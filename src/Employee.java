fb752ab6446ba66a3d92fe07b6ec4241036e34a9
TopalMahmutAli <mhmt_ali95@hotmail.com>
2026-09-15 10:27:51 +0200

    feat: implemented class Employee

--- src/Employee.java
+++ src/Employee.java
@@ -1,2 +1,36 @@
+import java.util.UUID;
+
 public class Employee {
+    private final UUID id;
+    private String lastname;
+    private String name;
+
+    public Employee() {
+    }
+
+    public Employee(UUID id, String lastname, String name) {
+        this.id = id;
+        this.lastname = lastname;
+        this.name = name;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
