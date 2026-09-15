2c0d76cafc1558aacbfe68587b00c849c0f887f6
TopalMahmutAli <mhmt_ali95@hotmail.com>
2026-09-15 10:17:14 +0200

    feat: implemented class Book and Customer

--- src/Book.java
+++ src/Book.java
@@ -1,2 +1,68 @@
 public class Book {
+    private int id;
+    private String title;
+    private float price;
+    private Author author;
+
+    private Category Category;
+    private int numberBook;
+
+    public Book() {}
+
+    public Book(int id, String title, float price, Author author, Category category, int numberBook) {
+        this.id = id;
+        this.title = title;
+        this.price = price;
+        this.author = author;
+        Category = category;
+        this.numberBook = numberBook;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public float getPrice() {
+        return price;
+    }
+
+    public void setPrice(float price) {
+        this.price = price;
+    }
+
+    public Author getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(Author author) {
+        this.author = author;
+    }
+
+    public Category getCategory() {
+        return Category;
+    }
+
+    public void setCategory(Category category) {
+        Category = category;
+    }
+
+    public int getNumberBook() {
+        return numberBook;
+    }
+
+    public void setNumberBook(int numberBook) {
+        this.numberBook = numberBook;
+    }
 }
--- src/Customer.java
+++ src/Customer.java
@@ -1,2 +1,46 @@
+import java.util.List;
+
 public class Customer {
+    private int id;
+    private String name;
+    private String lastName;
+    private List<Book> book;
+
+    public Customer(){};
+
+    public Customer(int id, String name, String lastName, List<Book> book){
+        this.id = id;
+        this.name = name;
+        this.lastName = lastName;
+        this.book = book;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public List<Book> getBook() {
+        return book;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+    public void setBook(List<Book> book) {
+        this.book = book;
+    }
 }
